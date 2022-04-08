e216cb314df46ae0c17be9e0a444ae4c02b25214
MorozDS <morozddima@gmail.com>
2022-04-08 21:31:40 +0300

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyColorEl = document.querySelector("body");
+
+let timerId = null;
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    bodyColorEl.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
